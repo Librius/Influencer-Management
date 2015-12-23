cae9c9bf6b22195b4f25c6a10e73b95eecd61b40
qlyao <qingleyao@gmail.com>
2015-12-22 16:08:42 -0800

    upload to dev

--- uploadfile.php
+++ uploadfile.php
@@ -4,6 +4,7 @@ $target_file = $target_dir. basename($_FILES["fileToUpload"]["name"]);
 while (file_exists($target_file)) {
   $target_file = substr($target_file,0,strrpos($target_file,".",-1))."0".substr($target_file,strrpos($target_file,".",-1));
 }
+
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
 {
   echo "http://".$_SERVER['HTTP_HOST'].substr($_SERVER['REQUEST_URI'],0,strrpos($_SERVER['REQUEST_URI'],"/",-1))."/".$target_file;
