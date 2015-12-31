f49d08017f181adc624c26dc89878fabd12676de
librius <bodu@usc.edu>
2015-12-30 17:54:22 -0800

    read and write json

--- event-management.js
+++ event-management.js
@@ -6,8 +6,42 @@ var singleEventJsonObj;
 var events;
 var tags = [];
 
-function readFromJson(){
+function getChunk(imgSrc, mainTitle, subTitle, link){
+    return "<div class=\"chunk_div\">" +
+    "<input type=\"file\" class=\"fileUpload\" onchange='UpLoadFile(event)'>" +
+    "<img class=\"entry_picture\" src=\"" + imgSrc + "\" alt=\"No file chosen\"/>" +
+    "Main Title: <input type=\"text\" class=\"entry_input\" value=\""+ mainTitle +"\">" +
+    "Subtitle: <input type=\"text\" class=\"entry_input\" value=\""+ subTitle +"\">" +
+    "Link: <input type=\"text\" class=\"entry_input\" value=\""+ link +"\">" +
+    "<button class=\"remove_chunk_buttons\" onclick=\"test()\">Remove</button>" +
+    "</div>"
+}
 
+var eventsJsonObj = {
+    "id":0,
+    "name": "Test Name",
+    "time_begin":"2015-01-01T01:00",
+    "time_end":"2015-12-31T12:59",
+    "brief":"lalalala brief description",
+    "event_hashtag": ["tag1", "tag2", "tag3"],
+    "big_image":[{"url": "#","main_title":"b1m","subtitle":"b1s","link":"b1l"},{"url": "#","main_title":"b2m","subtitle":"b2s","link":"b2l"}],
+    "middle_image":[{"url": "#","main_title":"m1m","subtitle":"m1s","link":"m1l"},{"url": "#","main_title":"m2m","subtitle":"m2s","link":"m2l"}]
+}
+
+function readFromJson(){
+    jQuery("#modal_name_input").val(eventsJsonObj.name);
+    jQuery("#modal_begin_time").val(eventsJsonObj.time_begin);
+    jQuery("#modal_end_time").val(eventsJsonObj.time_end);
+    jQuery("#modal_intro").val(eventsJsonObj.brief);
+    for(var i=0; i<eventsJsonObj.event_hashtag.length; i++) jQuery("#tag_pool").prepend("<button class=\"tag_button btn btn-default\">" + eventsJsonObj.event_hashtag[i] + "</button>");
+    for(var i=0; i<eventsJsonObj.big_image.length; i++) {
+        var bigImgJson = eventsJsonObj.big_image[i]
+        jQuery("#big_img_div").append(getChunk(bigImgJson.url, bigImgJson.main_title, bigImgJson.subtitle, bigImgJson.link));
+    }
+    for(var i=0; i<eventsJsonObj.middle_image.length; i++) {
+        var middleImgJson = eventsJsonObj.middle_image[i]
+        jQuery("#middle_img_div").append(getChunk(middleImgJson.url, middleImgJson.main_title, middleImgJson.subtitle, middleImgJson.link));
+    }
 }
 
 function writeToJson(){
@@ -58,14 +92,7 @@ jQuery(document).ready(function() {
 
     jQuery("#big_img_add_btn").click(function(){
         jQuery("#big_img_div").append(
-            "<div class=\"chunk_div\">" +
-            "<input type=\"file\" class=\"fileUpload\" onchange='UpLoadFile(event)'>" +
-            "<img class=\"entry_picture\" src=\"\" alt=\"No file chosen\"/>" +
-            "Main Title: <input type=\"text\" class=\"entry_input\">" +
-            "Subtitle: <input type=\"text\" class=\"entry_input\">" +
-            "Link: <input type=\"text\" class=\"entry_input\">" +
-            "<button class=\"remove_chunk_buttons\" onclick=\"test()\">Remove</button>" +
-            "</div>"
+            getChunk("", "", "", "")
         );
     });
 
