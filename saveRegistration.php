d457f23221f24b637b747afa25a525e9976e7bea
librius <bodu@usc.edu>
2016-01-05 11:52:23 -0800

    php

--- saveRegistration.php
+++ saveRegistration.php
@@ -1,78 +1,78 @@
 <?php
 
-$myfile = fopen("allInfluencer.txt", "w") or die("Unable to open file!");
-fwrite($myfile, "test");
+$myfile = fopen("allInfluencer.txt", "r") or die("Unable to open file!");
+$file_json = json_decode(fread($myfile,filesize("allInfluencer.txt")));
 fclose($myfile);
-//$result = array("status"=>"success","message"=>"OK.");
-//echo json_encode($result);
 
-if(isset($_POST["data"]))
+//validation
+if(!isset($_POST["firstname"]) || trim($_POST["firstname"]) == "")
 {
-    $data = json_decode($_POST["data"]);
-
-//    for($i = 0;$i<count($data->influencers);$i++)
-//    {
-//        if(!isset($data->influencers[$i]->first_name) || trim($data->influencers[$i]->first_name) == "")
-//        {
-//            $result = array("status"=>"error","message"=>"Please write the first name of every influencer.");
-//            echo json_encode($result);
-//            return;
-//        }
-//        if(!isset($data->influencers[$i]->last_name) || trim($data->influencers[$i]->last_name) == "")
-//        {
-//            $result = array("status"=>"error","message"=>"Please write the last name of every influencer.");
-//            echo json_encode($result);
-//            return;
-//        }
-//        if(!isset($data->influencers[$i]->blog) || trim($data->influencers[$i]->blog) == "")
-//        {
-//            $result = array("status"=>"error","message"=>"Please write the blog of every influencer.");
-//            echo json_encode($result);
-//            return;
-//        }
-//        if(!isset($data->influencers[$i]->facebook) || trim($data->influencers[$i]->facebook) == "")
-//        {
-//            $result = array("status"=>"error","message"=>"Please write the facebook of every influencer.");
-//            echo json_encode($result);
-//            return;
-//        }
-//        if(!isset($data->influencers[$i]->instagram) || trim($data->influencers[$i]->instagram) == "")
-//        {
-//            $result = array("status"=>"error","message"=>"Please write the instagram of every influencer.");
-//            echo json_encode($result);
-//            return;
-//        }
-//        if(!isset($data->influencers[$i]->youtube) || trim($data->influencers[$i]->youtube) == "")
-//        {
-//            $result = array("status"=>"error","message"=>"Please write the youtube of every influencer.");
-//            echo json_encode($result);
-//            return;
-//        }
-//        if(!isset($data->influencers[$i]->country) || trim($data->influencers[$i]->country) == "")
-//        {
-//            $result = array("status"=>"error","message"=>"Please write the country of every influencer.");
-//            echo json_encode($result);
-//            return;
-//        }
-//        if(!isset($data->influencers[$i]->email) || trim($data->influencers[$i]->email) == "")
-//        {
-//            $result = array("status"=>"error","message"=>"Please write the email of every influencer.");
-//            echo json_encode($result);
-//            return;
-//        }
-//        if(!isset($data->influencers[$i]->status) || trim($data->influencers[$i]->status) == "")
-//        {
-//            $result = array("status"=>"error","message"=>"Please write the status of every influencer.");
-//            echo json_encode($result);
-//            return;
-//        }
-//    }
-
-    $myfile = fopen("allInfluencer.txt", "w") or die("Unable to open file!");
-    fwrite($myfile, $_POST["data"]);
-    fclose($myfile);
-    $result = array("status"=>"success","message"=>"OK.");
-    echo json_encode($result);
+    echo "<script>alert(\"Please write the first name.\");</script>";
+    return;
+}
+if(!isset($_POST["lastname"]) || trim($_POST["lastname"]) == "")
+{
+    echo "<script>alert(\"Please write the last name.\");</script>";
+    return;
+}
+if(!isset($_POST["blogurl"]) || trim($_POST["blogurl"]) == "")
+{
+    echo "<script>alert(\"Please write the blog url.\");</script>";
+    return;
+}
+if(!isset($_POST["facebookaccount"]) || trim($_POST["facebookaccount"]) == "")
+{
+    echo "<script>alert(\"Please write the.\");</script>";
+    return;
+}
+if(!isset($_POST["instagramaccount"]) || trim($_POST["instagramaccount"]) == "")
+{
+    echo "<script>alert(\"Please write the.\");</script>";
+    return;
+}
+if(!isset($_POST["youtubeaccount"]) || trim($_POST["youtubeaccount"]) == "")
+{
+    echo "<script>alert(\"Please write the.\");</script>";
+    return;
+}
+if(!isset($_POST["otheraccount"]) || trim($_POST["otheraccount"]) == "")
+{
+    echo "<script>alert(\"Please write the.\");</script>";
+    return;
+}
+if(!isset($_POST["country"]) || trim($_POST["country"]) == "")
+{
+    echo "<script>alert(\"Please write the.\");</script>";
+    return;
+}
+if(!isset($_POST["applieremail"]) || trim($_POST["applieremail"]) == "")
+{
+    echo "<script>alert(\"Please write the.\");</script>";
+    return;
 }
 
+$global_id = $file_json->global_available_id;
+$local_date = (new DateTime(now, new DateTimeZone('America/Los_Angeles')))->format('Y-m-dTH:i');
+$current_json = array('id' => $global_id,
+    'first_name' => $_POST["firstname"],
+    'last_name' => $_POST["lastname"],
+    'blog' => $_POST["blogurl"],
+    'facebook' => $_POST["facebookaccount"],
+    'instagram' => $_POST["instagramaccount"],
+    'youtube' => $_POST["youtubeaccount"],
+    'other_media' => $_POST["otheraccount"],
+    'country' => $_POST["country"],
+    'email' => $_POST["applieremail"],
+    'status' => "Unprocessed",
+    'apply_time' => $local_date);
+$file_json->global_available_id = $global_id + 1;
+array_push($file_json->influencers, $current_json);
+print_r(json_encode($file_json));
+
+//file_put_contents($myfile, json_encode($data));
+$myfile = fopen("allInfluencer.txt", "w") or die("Unable to open file!");
+fwrite($myfile, json_encode($file_json));
+fclose($myfile);
+
+header("Location: https://www.evestemptation.com/Affilant_Apply_Success?");
 ?>
